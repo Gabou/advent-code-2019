fc775766ab996c28a46c9ac7285abab0615f69d9
gabou <supergabou+github@outlook.fr>
2019-12-03 18:29:13 +0100

    test fail test

---
+++ Day1/src/test/java/advent/code/FailTest.java
@@ -0,0 +1,16 @@
+package advent.code;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+class FailTest {
+
+    @Test
+    void shouldBeFalse() {
+        assertThat(false).isTrue();
+    }
+}
